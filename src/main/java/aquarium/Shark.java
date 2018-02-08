c9bb39c31956d1132472f9981286907b19b914da
Kasey <KaseyColian@gmail.com>
2018-02-07 20:01:42 -0500

    Created constructor getters in Shark. Will pull into Aquatic Creature Interface once Penguin is finished

--- src/main/java/aquarium/Shark.java
+++ src/main/java/aquarium/Shark.java
@@ -1,9 +1,32 @@
 package aquarium;
 
 public class Shark {
+	private String name;
+	private String microchipNumber;
+	private String color;
 
-	public Shark(String string, String string2, String string3) {
-	
+	public Shark(String microchipNumber, String name, String color) {
+		this.microchipNumber = microchipNumber;
+		this.name = name;
+		this.color = color;
 	}
+//will pull into AquaticCreature Interface
+	public String getMicrochipNumber() {
+		return microchipNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getColor() {
+		return color;
+	}
+
+
+
+
+	
+	
+
+	
 
 }
--- src/test/java/aquarium/SharkTest.java
+++ src/test/java/aquarium/SharkTest.java
@@ -1,5 +1,30 @@
 package aquarium;
 
-public class SharkTest {
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
+public class SharkTest {
+@Test
+public void shouldReturnMicrochipNumber() {
+Shark underTest = new Shark("1111", "Fin", "Grey");
+String microchipNumber =underTest.getMicrochipNumber();
+assertEquals(microchipNumber, "1111");
+}
+@Test
+public void shouldReturnName() {
+	Shark underTest = new Shark ("1111", "Fin", "Grey");
+	String name = underTest.getName();
+	assertEquals(name, "Fin");
+}
+
+@Test
+public void shouldReturnColor() {
+	Shark underTest = new Shark ("1111", "Fin", "Grey");
+	String color = underTest.getColor();
+	assertEquals(color, "Grey");
+	
+}
 }
